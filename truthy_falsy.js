e40eb92567a352544746228b4f964f941e6c4334
Masuda Akter Tisha <masudatisha63@gmail.com>
2026-06-04 11:57:40 +0600

    modified

--- truthy_falsy.js
+++ truthy_falsy.js
@@ -5,3 +5,13 @@ if ('') {
 else {
     console.log('it is false');   
 }
+
+let arr = [1,2,3,4];
+const isExistThreeHundred = arr.find (el => el === 300);
+ if (isExistThreeHundred) {
+    console.log('triggered if block');
+ }
+ else {
+    console.log('triggered else block');
+    
+ }
