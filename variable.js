aa0d659eb8c3477b47e80fbcdc92e9466f2f6094
Masuda Akter Tisha <masudatisha63@gmail.com>
2026-06-04 08:50:37 +0600

    practice let and const

---
+++ variable.js
@@ -0,0 +1,14 @@
+let age = 23;
+age = 24;
+
+const name = 'Tisha';
+// name = 'tasif'; cant reassign value by using const
+
+const arr = [1, 2, 3, 4, 5];
+arr.push (200);
+console.log(arr);
+
+// arr = [200,34,7]; can not reassign value but can modified array by using push pop shift unshift
+// console.log(arr);
+
+ 
