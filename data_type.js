411cca3d0435ba124a6aaaa2339b7c3fc8a55daf
Masuda Akter Tisha <masudatisha63@gmail.com>
2026-06-04 09:36:25 +0600

    practice data type

---
+++ data_type.js
@@ -0,0 +1,10 @@
+// two types of data 
+// primitive ---> number, boolean, string, null, undefined
+// non-primitive ---> array, object, function
+
+function myFunc () {
+    console.log('hello');
+    
+}
+
+console.log(typeof myFunc);
