2f599a766d8652afc352200a14dbabb98eea4eda
Masuda Akter Tisha <masudatisha63@gmail.com>
2026-06-14 09:54:42 +0600

    practice ES6 import export

---
+++ index.js
@@ -0,0 +1,10 @@
+const name = 'hablu';
+const age = 25;
+
+// console.log(name, age);
+
+export {name, age}
+
+// single vabhe kichu pathaite chaile
+// export default name;
+
---
+++ index2.js
@@ -0,0 +1,5 @@
+import {name as myName, age as old} from "./index.js";
+// import myName from "./index.js";
+
+
+console.log(myName, old);
